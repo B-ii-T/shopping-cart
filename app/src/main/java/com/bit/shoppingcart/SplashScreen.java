d1aec81876bebb8547bb664bc00e7fd3d2bf9080
Abdelfatah <bnb.fatah@gmail.com>
2023-08-23 23:54:18 +0100

    User Interface: splash screen

---
+++ app/src/main/java/com/bit/shoppingcart/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.bit.shoppingcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private RelativeLayout splashLayout;
+    private ImageView appLogo;
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
