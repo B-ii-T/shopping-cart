2550b92c4a76d997d4b4398ed7702b2d5eca5a1e
Abdelfatah <bnb.fatah@gmail.com>
2023-08-23 23:27:42 +0100

    User Interface: layouts and fragments

---
+++ app/src/main/java/com/bit/shoppingcart/ListFragment.java
@@ -0,0 +1,26 @@
+package com.bit.shoppingcart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.list_list, container, false);
+        RecyclerView listRecycler = rootview.findViewById(R.id.list_recyclerview);
+        return rootview;
+    }
+}
--- app/src/main/java/com/bit/shoppingcart/MainActivity.java
+++ app/src/main/java/com/bit/shoppingcart/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bit.shoppingcart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        ListFragment listFragment = new ListFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, listFragment).commit();
     }
 }
