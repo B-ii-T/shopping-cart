6a8bae27f65afa4757b17ea6790a94dbcee476e2
Abdelfatah <bnb.fatah@gmail.com>
2023-08-25 15:16:32 +0100

    User Interface: edit list

--- app/src/main/java/com/bit/shoppingcart/ListAdapter.java
+++ app/src/main/java/com/bit/shoppingcart/ListAdapter.java
@@ -6,6 +6,9 @@ import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,8 +53,11 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
                 itemClickListener.onItemClick(currentList.getId(), currentList.getListName());
             }
         });
-        holder.itemView.setOnLongClickListener(v -> {
+        holder.deleteListBtn.setOnClickListener(v -> {
             showDeleteConfirmationDialog(v, currentList);
+        });
+        holder.itemView.setOnLongClickListener(v -> {
+            showUpdateListDialog(v, currentList);
             return true;
         });
         MainActivity.itemViewModel.getItemCount(currentList.getId()).observe((LifecycleOwner) context, new Observer<Integer>() {
@@ -79,6 +85,36 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
                 .show();
     }
 
+    private void showUpdateListDialog(View v, com.bit.shoppingcart.List list) {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(v.getContext());
+        LayoutInflater inflater = LayoutInflater.from(v.getContext());
+        View dialogView = inflater.inflate(R.layout.add_list_dialog, null);
+        builder.setView(dialogView);
+
+        EditText listNameInput = dialogView.findViewById(R.id.list_name_input);
+        Button createListBtn = dialogView.findViewById(R.id.create_list_btn);
+
+        listNameInput.setText(list.getListName());
+        createListBtn.setText("Save");
+
+        final androidx.appcompat.app.AlertDialog dialog = builder.create();
+        dialog.setCancelable(true);
+
+        createListBtn.setOnClickListener(V -> {
+            if(listNameInput.getText().toString().trim().isEmpty()){
+                listNameInput.setError("Required");
+            }else{
+                com.bit.shoppingcart.List updatedList = new com.bit.shoppingcart.List(listNameInput.getText().toString().trim());
+                updatedList.setId(list.getId());
+                MainActivity.listViewModel.updateList(updatedList);
+                Toast.makeText(v.getContext(), "List updated", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
     @Override
     public int getItemCount() {
         return lists.size();
@@ -91,11 +127,13 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView listName, listItemCount;
+        ImageView deleteListBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             listName = itemView.findViewById(R.id.list_name_textview);
             listItemCount = itemView.findViewById(R.id.list_item_count);
+            deleteListBtn = itemView.findViewById(R.id.delete_list_btn);
         }
     }
 }
